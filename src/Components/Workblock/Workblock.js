0afd6ff02b6d347b5d87a8055fd9c6b8fc14276a
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2022-05-03 11:32:00 -0400

    images now populated

--- src/Components/Workblock/Workblock.js
+++ src/Components/Workblock/Workblock.js
@@ -19,9 +19,9 @@ export default function Workblock() {
             projectTitle: "Faire L'amour Literature Club",
             projectStack: "Cargo. Click for live site.",
             projectPhotos: [
-                {image1:""},
-                {image2:""},
-                {image2:""},
+                {image1:"/images/fllc/Screen Shot 2022-05-02 at 20.15.49.png"},
+                {image2:"/images/fllc/Screen Shot 2022-05-02 at 20.16.15.png"},
+                {image3:"/images/fllc/Screen Shot 2022-05-02 at 20.16.01.png"},
             ],
             projectDate: "2020",
             projectLink: ''
@@ -30,9 +30,9 @@ export default function Workblock() {
             projectTitle: "TalkBack",
             projectStack: "Html, Css, Javascript, Axios. Click for code repository.",
             projectPhotos: [
-                {image1:""},
-                {image2:""},
-                {image2:""},
+                {image1:"/images/talkback/Screen Shot 2022-05-02 at 20.27.49.png"},
+                {image2:"/images/talkback/Screen Shot 2022-05-02 at 20.28.07.png"},
+                {image3:"/images/talkback/Screen Shot 2022-05-02 at 20.28.32.png"},
             ],
             projectDate: "2022",
             projectLink: ''
@@ -41,9 +41,9 @@ export default function Workblock() {
             projectTitle: "RoseHips Band Site",
             projectStack: "Cargo. Click for live site.",
             projectPhotos: [
-                {image1:""},
-                {image2:""},
-                {image2:""},
+                {image1:"/images/rosehips/Screen Shot 2022-04-21 at 13.26.15.png"},
+                {image2:"/images/rosehips/Untitled design (1).png"},
+                {image3:"/images/rosehips/Untitled design.png"},
             ],
             projectDate: "2022",
             projectLink: ''
@@ -52,9 +52,9 @@ export default function Workblock() {
             projectTitle: "Shopify Hackathon",
             projectStack: "React. Click for code repository.",
             projectPhotos: [
-                {image1:""},
-                {image2:""},
-                {image2:""},
+                {image1:"/images/shopify/Screen Shot 2022-05-02 at 20.18.41.png"},
+                {image2:"/images/shopify/Screen Shot 2022-05-02 at 20.19.15.png"},
+                {image3:"/images/shopify/Screen Shot 2022-05-02 at 20.19.31.png"},
             ],
             projectDate: "2022",
             projectLink: ''
@@ -63,9 +63,9 @@ export default function Workblock() {
             projectTitle: "Astrum",
             projectStack: "React, Axios, SASS, Node.js, Express.js, MongoDB, Atlas, Mongoose, JWT. Click for code repository.",
             projectPhotos: [
-                {image1:"../../images/astrum/tp238-pf-s73-05-mockup.png"},
-                {image2:"../../images/astrum/Screen Shot 2022-05-02 at 21.52.02.png"},
-                {image2:"../../images/astrum/Screen Shot 2022-04-06 at 22.26.04.png"},
+                {image1:"/images/astrum/tp238-pf-s73-05-mockup.png"},
+                {image2:"/images/astrum/Screen Shot 2022-05-02 at 21.52.02.png"},
+                {image3:"/images/astrum/Screen Shot 2022-04-06 at 22.26.04.png"},
             ],
             projectDate: "2022",
             projectLink: ''
@@ -74,14 +74,16 @@ export default function Workblock() {
             projectTitle: "WorkBlock",
             projectStack: "Html, SASS, Javascript, Document Object Model. Click for live application.",
             projectPhotos: [
-                {image1:""},
-                {image2:""},
-                {image2:""},
+                {image1:"/images/workblock/Screen Shot 2022-05-02 at 20.24.48.png"},
+                {image2:"/images/workblock/Screen Shot 2022-05-02 at 20.25.02.png"},
+                {image3:"/images/workblock/Screen Shot 2022-05-02 at 20.25.34.png"},
             ],
             projectDate: "2022",
             projectLink: ''
         },
     ]
+
+    console.log(projectData[4].projectPhotos[0].image1)
 
     return (
         <main className = 'projects'>
@@ -97,9 +99,9 @@ export default function Workblock() {
                     <div className = { selected === i ? 'projects__content-show' : 'projects__content'}>
                         <p className = 'projects__build'>Build:  {item.projectStack}</p>
                         <div className = 'projects__images'>
-                            <img className = 'image' src = {item.projectPhotos.image1} />
-                            <img className = 'image' src = {item.projectPhotos.image2} />
-                            <img className = 'image' src = {item.projectPhotos.image3} />
+                            <img className = 'image' src = {item.projectPhotos[0].image1} />
+                            <img className = 'image' src = {item.projectPhotos[1].image2} />
+                            <img className = 'image' src = {item.projectPhotos[2].image3} />
                         </div>
                     </div>
                     </div>
