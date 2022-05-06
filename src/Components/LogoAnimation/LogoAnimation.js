984fadb3513e7c57407d9836807d4516691663e4
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2022-05-05 22:58:17 -0400

    fixed contact form styling that was showing up poorly on iphone, and attempting to fix mp4 file not playing on iphone

--- src/Components/LogoAnimation/LogoAnimation.js
+++ src/Components/LogoAnimation/LogoAnimation.js
@@ -3,7 +3,7 @@ import './LogoAnimation.scss'
 export default function LogoAnimation() {
     return (
         <main className = 'container'>
-            <video id = 'video' loop autoPlay muted>
+            <video id = 'video' loop playsInline autoPlay muted>
                 <source src = '/Video.mp4' />
             </video>
         </main>
